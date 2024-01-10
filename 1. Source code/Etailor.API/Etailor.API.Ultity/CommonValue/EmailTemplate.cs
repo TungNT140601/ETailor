8d79a4660846ce96efa2eafa7d3dd34a2a61500f
TT Nguyen <tungnt14062001@gmail.com>
2024-01-10 12:42:19 +0700

    up code 01/10/24

---
+++ 1. Source code/Etailor.API/Etailor.API.Ultity/CommonValue/EmailTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etailor.API.Ultity.CommonValue
+{
+    public static class EmailOTPTemplate
+    {
+        public static string Header = "";
+
+    }
+}
