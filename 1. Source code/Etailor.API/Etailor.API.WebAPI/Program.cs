275bcccc0326366253aa2621b45cd7a61dbe73bc
TungNT140601 <tungnt14062001@gmail.com>
2024-01-19 20:01:30 +0700

    add code 01192024 2001

--- 1. Source code/Etailor.API/Etailor.API.WebAPI/Program.cs
+++ 1. Source code/Etailor.API/Etailor.API.WebAPI/Program.cs
@@ -81,10 +81,12 @@ builder.Services.AddAuthentication(option =>
 
 builder.Services.AddScoped<ISkillRepository, SkillRepository>();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-
-builder.Services.AddScoped<ISkillRepository, SkillRepository>();
 builder.Services.AddScoped<IStaffRepository, StaffRepository>();
+builder.Services.AddScoped<ICustomerClientRepository, CustomerClientRepository>();
+
+
+builder.Services.AddScoped<ISkillService, SkillService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IStaffService, StaffService>();
 
 
