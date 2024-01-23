a1f27fda2703a5bdace263d24f59d70f76e3fd78
TT Nguyen <tungnt14062001@gmail.com>
2024-01-23 10:23:18 +0700

    add code category

---
+++ 1. Source code/Etailor.API/Etailor.API.Service/Interface/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Etailor.API.Repository.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etailor.API.Service.Interface
+{
+    public interface ICategoryService
+    {
+        bool AddCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(string id);
+        Category GetCategory(string id);
+        IEnumerable<Category> GetCategorys(string? search);
+    }
+}
