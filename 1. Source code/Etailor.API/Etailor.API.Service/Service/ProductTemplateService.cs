33ed91816806297405f04140253d4da6ce3878cf
TT Nguyen <tungnt14062001@gmail.com>
2024-01-25 16:06:28 +0700

    add code

---
+++ 1. Source code/Etailor.API/Etailor.API.Service/Service/ProductTemplateService.cs
@@ -0,0 +1,217 @@
+﻿using Etailor.API.Repository.EntityModels;
+using Etailor.API.Repository.Interface;
+using Etailor.API.Service.Interface;
+using Etailor.API.Ultity;
+using Etailor.API.Ultity.CustomException;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Etailor.API.Service.Service
+{
+    public class ProductTemplateService : IProductTemplateService
+    {
+        private readonly IProductTemplateRepository productTemplateRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public ProductTemplateService(IProductTemplateRepository productTemplateRepository, ICategoryRepository categoryRepository)
+        {
+            this.productTemplateRepository = productTemplateRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public async Task<bool> AddTemplate(ProductTemplate productTemplate, string wwwroot, List<IFormFile>? images, List<IFormFile>? collectionImages)
+        {
+            var genId = Task.Run(() =>
+            {
+                productTemplate.Id = Ultils.GenGuidString();
+            });
+
+            var checkCategoryId = Task.Run(() =>
+            {
+                if (productTemplate.CategoryId == null)
+                {
+                    throw new UserException("Vui lòng chọn loại danh mục");
+                }
+                else if (categoryRepository.Get(productTemplate.CategoryId) == null || categoryRepository.Get(productTemplate.CategoryId).IsActive != true)
+                {
+                    throw new UserException("Loại danh mục không tồn tại");
+                }
+            });
+
+            var checkName = Task.Run(() =>
+            {
+                if (string.IsNullOrWhiteSpace(productTemplate.Name))
+                {
+                    throw new UserException("Vui lòng nhập tên cho mẫu sản phẩm");
+                }
+                else
+                {
+                    productTemplate.UrlPath = Ultils.ConvertToEnglishAlphabet(productTemplate.Name);
+                    if (productTemplateRepository.GetAll(x => (x.Name == productTemplate.Name || x.UrlPath == productTemplate.UrlPath) && x.IsActive == true).Any())
+                    {
+                        throw new UserException($"Tên mẫu sản phẩm đã được sử dụng: {productTemplate.UrlPath.Replace("-", " ")}");
+                    }
+                }
+            });
+
+            var setDesc = Task.Run(() =>
+            {
+                if (string.IsNullOrWhiteSpace(productTemplate.Description))
+                {
+                    productTemplate.Description = "";
+                }
+            });
+            var checkPrice = Task.Run(() =>
+            {
+                if (productTemplate.Price == null || productTemplate.Price == 0)
+                {
+                    throw new UserException("Vui lòng nhập giá tham khảo sản phẩm cho mẫu sản phẩm");
+                }
+                else if (productTemplate.Price < 0)
+                {
+                    throw new UserException("Giá tham khảo không phù hợp");
+                }
+            });
+            var setImage = Task.Run(() =>
+            {
+                if (images != null)
+                {
+                    var imagesUrls = Ultils.UploadImages(wwwroot, "TemplateImages", images);
+
+                    productTemplate.Image = JsonSerializer.Serialize(imagesUrls);
+                }
+            });
+            var setCoolectionImage = Task.Run(async () =>
+            {
+                if (collectionImages != null)
+                {
+                    var imagesUrls = await Ultils.UploadImages(wwwroot, "TemplateCollectionImages", collectionImages);
+
+                    productTemplate.CollectionImage = JsonSerializer.Serialize(imagesUrls);
+                }
+            });
+            var setValue = Task.Run(() =>
+            {
+                productTemplate.CreatedTime = DateTime.Now;
+                productTemplate.LastestUpdatedTime = DateTime.Now;
+                productTemplate.InactiveTime = null;
+                productTemplate.IsActive = true;
+            });
+
+            await Task.WhenAll(genId, checkCategoryId, checkName, setDesc, checkPrice, setImage, setCoolectionImage, setValue);
+
+            return productTemplateRepository.Create(productTemplate);
+        }
+
+        public async Task<bool> UpdateTemplate(ProductTemplate productTemplate, string wwwroot, List<IFormFile>? images, List<IFormFile>? collectionImages)
+        {
+            var dbTemplate = productTemplateRepository.Get(productTemplate.Id);
+
+            if (dbTemplate != null)
+            {
+                var checkCategoryId = Task.Run(() =>
+                {
+                    if (productTemplate.CategoryId == null)
+                    {
+                        throw new UserException("Vui lòng chọn loại danh mục");
+                    }
+                    else if (categoryRepository.Get(productTemplate.CategoryId) == null || categoryRepository.Get(productTemplate.CategoryId).IsActive != true)
+                    {
+                        throw new UserException("Loại danh mục không tồn tại");
+                    }
+                    else
+                    {
+                        dbTemplate.CategoryId = productTemplate.CategoryId;
+                    }
+                });
+
+                var checkName = Task.Run(() =>
+                {
+                    if (string.IsNullOrWhiteSpace(productTemplate.Name))
+                    {
+                        throw new UserException("Vui lòng nhập tên cho mẫu sản phẩm");
+                    }
+                    else
+                    {
+                        productTemplate.UrlPath = Ultils.ConvertToEnglishAlphabet(productTemplate.Name);
+                        if (productTemplateRepository.GetAll(x => x.Id != dbTemplate.Id && (x.Name == productTemplate.Name || x.UrlPath == productTemplate.UrlPath) && x.IsActive == true).Any())
+                        {
+                            throw new UserException($"Tên mẫu sản phẩm đã được sử dụng: {productTemplate.UrlPath.Replace("-", " ")}");
+                        }
+                        else
+                        {
+                            dbTemplate.Name = productTemplate.Name;
+                            dbTemplate.UrlPath = productTemplate.UrlPath;
+                        }
+                    }
+                });
+
+                var setDesc = Task.Run(() =>
+                {
+                    if (string.IsNullOrWhiteSpace(productTemplate.Description))
+                    {
+                        productTemplate.Description = "";
+                    }
+                    else
+                    {
+                        dbTemplate.Description = productTemplate.Description;
+                    }
+                });
+                var checkPrice = Task.Run(() =>
+                {
+                    if (productTemplate.Price == null || productTemplate.Price == 0)
+                    {
+                        throw new UserException("Vui lòng nhập giá tham khảo sản phẩm cho mẫu sản phẩm");
+                    }
+                    else if (productTemplate.Price < 0)
+                    {
+                        throw new UserException("Giá tham khảo không phù hợp");
+                    }
+                    else
+                    {
+                        dbTemplate.Price = productTemplate.Price;
+                    }
+                });
+                var setImage = Task.Run(async () =>
+                {
+                    if (images != null)
+                    {
+                        var imagesUrls = await Ultils.UploadImages(wwwroot, "TemplateImages", images);
+                        if (!string.IsNullOrWhiteSpace(dbTemplate.Image))
+                        {
+                            imagesUrls.AddRange(JsonSerializer.Deserialize<List<string>>(dbTemplate.Image));
+                        }
+                        dbTemplate.Image = JsonSerializer.Serialize(imagesUrls);
+                    }
+                });
+                var setCoolectionImage = Task.Run(() =>
+                {
+                    if (collectionImages != null)
+                    {
+                        var imagesUrls = Ultils.UploadImages(wwwroot, "TemplateCollectionImages", collectionImages);
+
+                        productTemplate.CollectionImage = JsonSerializer.Serialize(imagesUrls);
+                    }
+                });
+                var setValue = Task.Run(() =>
+                {
+                    dbTemplate.LastestUpdatedTime = DateTime.Now;
+                    dbTemplate.InactiveTime = null;
+                    dbTemplate.IsActive = true;
+                });
+
+                await Task.WhenAll(checkCategoryId, checkName, setDesc, checkPrice, setImage, setCoolectionImage, setValue);
+                return productTemplateRepository.Update(dbTemplate.Id, dbTemplate);
+            }
+            else
+            {
+                throw new UserException("Mẫu sản phẩm không tìm thấy");
+            }
+        }
+    }
+}
--- 1. Source code/Etailor.API/Etailor.API.WebAPI/Controllers/ComponentTypeController.cs
+++ 1. Source code/Etailor.API/Etailor.API.WebAPI/Controllers/ComponentTypeController.cs
@@ -27,7 +27,7 @@ namespace Etailor.API.WebAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddCategory([FromBody] ComponentTypeFormVM componentType)
+        public async Task<IActionResult> AddComponentType([FromBody] ComponentTypeFormVM componentType)
         {
             try
             {
@@ -69,7 +69,7 @@ namespace Etailor.API.WebAPI.Controllers
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateCategory(string? id, [FromBody] ComponentTypeFormVM componentType)
+        public async Task<IActionResult> UpdateComponentType(string? id, [FromBody] ComponentTypeFormVM componentType)
         {
             try
             {
@@ -102,6 +102,112 @@ namespace Etailor.API.WebAPI.Controllers
                         }
                     }
                 }
+            }
+            catch (UserException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (SystemsException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteComponentType(string? id)
+        {
+            try
+            {
+                var role = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+                if (role == null)
+                {
+                    return Unauthorized("Chưa đăng nhập");
+                }
+                else if (role != RoleName.MANAGER)
+                {
+                    return Forbid("Không có quyền truy cập");
+                }
+                else
+                {
+                    var staffid = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+                    var secrectKey = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.CookiePath)?.Value;
+                    if (!staffService.CheckSecrectKey(staffid, secrectKey))
+                    {
+                        return Unauthorized("Chưa đăng nhập");
+                    }
+                    else
+                    {
+                        if (id == null)
+                        {
+                            return NotFound("Không tìm thấy loại danh mục");
+                        }
+                        else
+                        {
+                            return componentTypeService.DeleteComponentType(id) ? Ok("Xóa loại danh mục thành công") : BadRequest("Xóa loại danh mục thất bại");
+                        }
+                    }
+                }
+            }
+            catch (UserException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (SystemsException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetComponentType(string? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return NotFound("Không tìm thấy loại danh mục");
+                }
+                else
+                {
+                    var componentType = componentTypeService.GetComponentType(id);
+                    if (componentType == null)
+                    {
+                        return NotFound("Không tìm thấy loại danh mục");
+                    }
+                    else
+                    {
+                        return Ok(mapper.Map<ComponentTypeVM>(componentType));
+                    }
+                }
+            }
+            catch (UserException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (SystemsException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetComponentTypes(string? search)
+        {
+            try
+            {
+                return Ok(mapper.Map<IEnumerable<ComponentTypeVM>>(componentTypeService.GetComponentTypes(search)));
             }
             catch (UserException ex)
             {
