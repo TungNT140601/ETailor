7be55feeedf87e412960a46cb1359b59e8090023
MinhhTris2909 <minhtrimt290901@gmail.com>
2024-01-28 00:26:25 +0700

    add file repo, ser of product table

--- 1. Source code/Etailor.API/Etailor.API.Service/Service/ProductService.cs
+++ 1. Source code/Etailor.API/Etailor.API.Service/Service/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using Etailor.API.Service.Interface;
+﻿using Etailor.API.Repository.EntityModels;
+using Etailor.API.Repository.Interface;
+using Etailor.API.Service.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,26 @@ namespace Etailor.API.Service.Service
 {
     public class ProductService : IProductService
     {
+        private readonly IProductRepository productRepository;
+        private readonly IProductTemplateRepository productTemplateRepository;
+        private readonly IOrderRepository orderRepository;
 
+        public ProductService(IProductRepository productRepository, IProductTemplateRepository productTemplateRepository, IOrderRepository orderRepository)
+        {
+            this.productRepository = productRepository;
+            this.productTemplateRepository = productTemplateRepository;
+            this.orderRepository = orderRepository;
+        }
+
+        public Product GetProduct(string id)
+        {
+            var product = productRepository.Get(id);
+            return product == null ? null : product.IsActive == true ? product : null;
+        }
+
+        public IEnumerable<Product> GetProductsByOrderId(string? search)
+        {
+            return productRepository.GetAll(x => ((search != null && x.OrderId.Trim().ToLower().Contains(search.ToLower().Trim()))) && x.IsActive == true);
+        }
     }
 }
