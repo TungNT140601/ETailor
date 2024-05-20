125de7c57a4c3e99d91006192067aafca3220296
TungNT140601 <tungnt14062001@gmail.com>
2024-05-20 22:57:55 +0700

    save code

--- 2. Database/Scripts/UpdateDateTimeToPre30Days.sql
+++ 2. Database/Scripts/UpdateDateTimeToPre30Days.sql
@@ -8,15 +8,17 @@ FROM INFORMATION_SCHEMA.COLUMNS c
 JOIN INFORMATION_SCHEMA.TABLES t ON c.TABLE_NAME = t.TABLE_NAME
 WHERE c.DATA_TYPE = 'datetime' AND t.TABLE_TYPE = 'BASE TABLE'
 
+EXECUTE [dbo].[CheckNumOfDateToFinish];
+
 OPEN TableCursor
 
 FETCH NEXT FROM TableCursor INTO @TableName, @ColumnName
 WHILE @@FETCH_STATUS = 0
 BEGIN
     SET @SQL = 'UPDATE ' + QUOTENAME(@TableName) + 
-               ' SET ' + QUOTENAME(@ColumnName) + ' = DATEADD(DAY, -30, ' + QUOTENAME(@ColumnName) + ')' +
+               ' SET ' + QUOTENAME(@ColumnName) + ' = DATEADD(MONTH, -2, ' +  + QUOTENAME(@ColumnName) + ')' +
                ' WHERE ' + QUOTENAME(@ColumnName) + ' IS NOT NULL' +
-               ' AND ' + QUOTENAME(@ColumnName) + ' > DATEADD(DAY, -30,  ' + QUOTENAME(@ColumnName) + ');'
+               ' AND MONTH(' + QUOTENAME(@ColumnName) + ') = 5;'
     PRINT @SQL;
 
     EXEC sp_executesql @SQL;
@@ -27,9 +29,3 @@ END
 CLOSE TableCursor
 DEALLOCATE TableCursor
 
-UPDATE [Order] SET PlannedTime = NULL;
-
-DECLARE @RC int
-
-EXECUTE @RC = [dbo].[CheckNumOfDateToFinish] 
-GO
